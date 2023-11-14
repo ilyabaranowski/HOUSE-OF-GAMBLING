7498fe2f35bff63389ce8bf7c49837ea809ba8ac
ilya baranouski <iliuxa32@gmail.com>
2023-11-14 11:58:44 +0300

    refactor: add prettier

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 //Burger menu appears on click
-const burger = document.querySelector('.burger');
-const navMenu = document.querySelector('.navigation__list');
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".navigation__list");
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll('.nav-link').forEach(nav =>
-nav.addEventListener('click', () => {
-    burger.classList.remove('active');
-    navMenu.classList.remove('active');
-}))
+document.querySelectorAll(".nav-link").forEach((nav) =>
+  nav.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }),
+);
 
 //Text changing on button language
 
-const changeLang = document.querySelector('.globe-txt');
+const changeLang = document.querySelector(".globe-txt");
 
-changeLang.addEventListener('click', () => {
-    changeLang.innerHTML = 'RU';
+changeLang.addEventListener("click", () => {
+  changeLang.innerHTML = "RU";
 });
-
-
